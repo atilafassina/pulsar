8c4167865e6f11b618ff910444b1e3d5acbf370b
Fabien BERNARD <fabien0102@hotmail.com>
2023-08-31 14:03:38 +0200

    Connect to specta

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,9 +7,12 @@ use error::Error;
 use fs_extra::dir::{self};
 use nmw::FolderStat;
 use rayon::prelude::*;
+use specta::collect_types;
+use tauri_specta::ts;
 use tokio::sync::oneshot;
 
 #[tauri::command]
+#[specta::specta]
 async fn get_dir_data(pattern: &str) -> Result<Vec<FolderStat>, Error> {
     let (tx, rx) = oneshot::channel::<Vec<FolderStat>>();
     let files = nmw::get_file_names(pattern);
@@ -29,6 +32,9 @@ async fn get_dir_data(pattern: &str) -> Result<Vec<FolderStat>, Error> {
 }
 
 fn main() {
+    #[cfg(debug_assertions)]
+    ts::export(collect_types![get_dir_data], "../src/commands.ts").unwrap();
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![get_dir_data])
         .run(tauri::generate_context!())
