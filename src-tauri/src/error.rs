74bf149926d8aee9d991ad1d4cb6f8b603af76c1
Jonas Kruckenberg <iterpre@protonmail.com>
2023-09-26 09:40:19 +0200

    fix error handling and use async get_size

--- src-tauri/src/error.rs
+++ src-tauri/src/error.rs
@@ -1,18 +1,20 @@
 use serde::{Serialize, Serializer};
+use crate::FolderStat;
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
-    #[error("Failed to receive data from node_modules")]
-    ReceiveDirData(#[from] tokio::sync::oneshot::error::RecvError),
-
     #[error("Tokio can't readdir")]
     Io(#[from] std::io::Error),
+    #[error("Failed to forward folder statistics, internal channel is broken.")]
+    TrySendError(#[from] tokio::sync::mpsc::error::TrySendError<FolderStat>),
+    #[error("Node modules folder size too large to be represented in JavaScript.")]
+    TooLarge(#[from] std::num::TryFromIntError),
 }
 
 impl Serialize for Error {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
+        where
+            S: Serializer,
     {
         serializer.serialize_str(self.to_string().as_ref())
     }
--- src-tauri/src/util.rs
+++ src-tauri/src/util.rs
@@ -1,18 +1,16 @@
-mod error;
-
 use globwalk::GlobWalkerBuilder;
 // use log::info;
 use regex::Regex;
-use std::path::Path;
-
+use std::path::{Path, PathBuf};
 use core::future::Future;
 use core::pin::Pin;
 use serde::Serialize;
 use specta::Type;
+use crate::error::Error;
 
 #[derive(Serialize, Type, Debug)]
 pub struct FolderStat {
-    pub path: String,
+    pub path: PathBuf,
     pub size: u32,
 }
 
@@ -23,25 +21,25 @@ pub fn order_list(mut list: Vec<FolderStat>) -> Vec<FolderStat> {
     list
 }
 
-pub fn get_size<'a>(path: &'a Path) -> Pin<Box<dyn Future<Output = u64> + 'a>> {
+pub fn get_size<'a>(path: &'a Path) -> Pin<Box<dyn Future<Output = Result<u64, Error>> + 'a + Send>> {
     Box::pin(async move {
         // Using `fs::symlink_metadata` since we don't want to follow symlinks,
         // as we're calculating the exact size of the requested path itself.
-        let path_metadata = tokio::fs::symlink_metadata(&path).await.unwrap();
+        let path_metadata = tokio::fs::symlink_metadata(&path).await?;
         let mut size_in_bytes = 0;
 
         if path_metadata.is_dir() {
-            let mut read_dir = tokio::fs::read_dir(&path).await.unwrap();
+            let mut read_dir = tokio::fs::read_dir(&path).await?;
 
-            while let Some(entry) = read_dir.next_entry().await.unwrap() {
+            while let Some(entry) = read_dir.next_entry().await? {
                 // `DirEntry::metadata` does not follow symlinks (unlike `fs::metadata`), so in the
                 // case of symlinks, this is the size of the symlink itself, not its target.
-                let entry_metadata = entry.metadata().await.unwrap();
+                let entry_metadata = entry.metadata().await?;
 
                 if entry_metadata.is_dir() {
                     // The size of the directory entry itself will be counted inside the `get_size()` call,
                     // so we intentionally don't also add `entry_metadata.len()` to the total here.
-                    size_in_bytes += get_size(&entry.path()).await;
+                    size_in_bytes += get_size(&entry.path()).await?;
                 } else {
                     size_in_bytes += entry_metadata.len();
                 }
@@ -49,12 +47,12 @@ pub fn get_size<'a>(path: &'a Path) -> Pin<Box<dyn Future<Output = u64> + 'a>> {
         } else {
             size_in_bytes = path_metadata.len();
         }
-        size_in_bytes
+        Ok(size_in_bytes)
     })
 }
 
-pub fn get_dir_names(path: &str) -> Vec<String> {
-    let mut filenames: Vec<String> = Vec::new();
+pub fn get_dir_names(path: &Path) -> Vec<PathBuf> {
+    let mut filenames = Vec::new();
 
     let walker = GlobWalkerBuilder::from_patterns(path, &["**/node_modules", "**/![.pnpm]/*"])
         .max_depth(6)
@@ -83,7 +81,7 @@ pub fn get_dir_names(path: &str) -> Vec<String> {
         }
 
         if should_scan {
-            filenames.push(path_string)
+            filenames.push(file.path().to_path_buf())
         }
     }
 
