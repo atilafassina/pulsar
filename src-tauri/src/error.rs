7ec1b7f5a21148727bfaddf94906c5ac671902b0
Jonas Kruckenberg <iterpre@protonmail.com>
2023-09-26 09:48:30 +0200

    fmt

--- src-tauri/src/error.rs
+++ src-tauri/src/error.rs
@@ -1,5 +1,5 @@
-use serde::{Serialize, Serializer};
 use crate::FolderStat;
+use serde::{Serialize, Serializer};
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
@@ -13,8 +13,8 @@ pub enum Error {
 
 impl Serialize for Error {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-        where
-            S: Serializer,
+    where
+        S: Serializer,
     {
         serializer.serialize_str(self.to_string().as_ref())
     }
